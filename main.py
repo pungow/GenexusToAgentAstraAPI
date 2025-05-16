e280950738a41808a237c7d40a9e3f1e9697cade
Wilmer Pungo <79926901+pungow@users.noreply.github.com>
2025-05-16 18:38:53 -0500

    Update port manually

--- main.py
+++ main.py
@@ -56,4 +56,5 @@ def upload_document(shipment_id):
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5050, threaded=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0"debug=True, port=port, threaded=True)
